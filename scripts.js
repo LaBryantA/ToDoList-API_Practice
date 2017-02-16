30926d1aa6bda81d6477463a1dcd866acc76d7ce
Bryant La <LaBryantA@gmail.com>
2017-02-15 22:14:34 -0700

    added ability to add and toggle completion

--- scripts.js
+++ scripts.js
@@ -1,50 +1,61 @@
 var toDoAPI = "https://nodetodolistapi.herokuapp.com/api/todos";
 
-function toggleCompletion(task){
-  var completionStatus = task.getAttribute(data-finished);
-  var taskID = task.getAttribute(data-ID);
-  var taskTitle = task.InnerHTML;
+function getList(){
   $.ajax({
-    url: todDoAPI + '/' + taskID,
-    type: 'PUT',
-    data: {
-      title: taskTitle,
-      finished: (!completionStatus)
+    url: toDoAPI,
+    success: function(load){
+      console.log(load);
+      for(var index = 0; index < load.length; index++){
+        if(load[index].finished  == true){
+          var task = "<p style='text-decoration:line-through;font-size:25px' onclick='toggleCompletion(this)' data-finished='true' data-ID='" + load[index]._id + "'>" + load[index].title + "</p>";
+        }
+        else
+        {
+          var task = "<p style='font-size:25px' onclick='toggleCompletion(this)' data-finished='false' data-ID='" + load[index]._id + "'>" + load[index].title + "</p>";
+        }
+        $(".todo-elements").append(task);
+      }
     }
   })
+}
+
+getList();
+
+function toggleCompletion(task){
+  console.log(task);
+  var completionStatus = task.getAttribute("data-finished");
+  var completionBool = !(completionStatus == 'true');
+  var taskID = task.getAttribute("data-ID");
+  var taskName = task.innerHTML;
+  console.log(taskName);
+  console.log(completionBool);
+  $.ajax({
+    url: toDoAPI + '/' + taskID,
+    type: 'PUT',
+    data: {
+      title: taskName,
+      finished: (completionBool)
+    }
+  })
+  $(".todo-elements").empty();
   getList();
 }
 
 $(document).ready(function(){
-  function getList(){
+  $(".text-button").click(function(){
+    var input = $(".task-input").val();
+    if(input){
       $.ajax({
         url: toDoAPI,
-        success: function(load){
-          console.log(load);
-          for(var index = 0; index < load.length; index++){
-            if(load[index].finished  == true){
-              var task = "<p style='text-decoration:line-through;font-size:25px' onclick='toggleCompletion(this)' data-finished='true' data-ID='" + load[index]._id + "'>" + load[index].title + "</p>";
-            }
-            else
-            {
-              var task = "<p style='font-size:25px' onclick='toggleCompletion(this)' data-finished='false' data-ID='" + load[index]._id + "'>" + load[index].title + "</p>";
-            }
-            $(".todo-elements").append(task);
-          }
+        type: 'POST',
+        data: {
+          title: input
+        },
+        success: function(){
+          var added = "<p style='font-size:25px' onclick='toggleCompletion(this)' data-finished='false'>" + input + "</p>";
+          $(".todo-elements").append(added);
         }
       })
-    getList();
-  }
-})
-
-getList();
-
-
-
-var texts = "<p data-finished='true' style='text-decoration:line-through;font-size:25px'>hi</p>";
-$(document).ready(function(){
-  $(".text-button").click(function(){
-    alert("hi");
-    $(".todo-elements").append(texts);
+    }
   })
 })
